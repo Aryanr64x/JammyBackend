34d30657adf47ff5d668f32497a91996e02e9f49
saket <aryansaket@yahoo.com>
2023-07-28 21:29:22 +0530

    bug fixed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import {Server} from 'socket.io';
 
 const io = new Server(server, {
     cors:{
-        origin: "http://localhost:3000"
+        origin: "https://jammy-backend.onrender.com"
     }
 });
 
